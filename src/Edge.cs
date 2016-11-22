281973f6005f4cfb24f0f2bb1268295f05ae9ee2
mariateresachaves <up201306842@fe.up.pt>
2016-11-22 17:35:32 +0000

    implementation begins. node + edge implemented.

---
+++ src/Edge.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeometryFriendsAgents
+{
+    class Edge
+    {
+        public Node source;
+        public Node dest;
+
+        public Edge(Node source, Node dest)
+        {
+            this.source = source;
+            this.dest = dest;
+        }
+
+        /******************/
+        /*      GETs      */
+        /******************/
+
+        public Node get_source()
+        {
+            return this.source;
+        }
+
+        public Node get_dest()
+        {
+            return this.dest;
+        }
+
+        /******************/
+        /*      SETs      */
+        /******************/
+
+        public void set_source(Node source)
+        {
+            this.source = source;
+        }
+
+        public void set_dest(Node dest)
+        {
+            this.dest = dest;
+        }
+    }
+}
---
+++ src/Node.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeometryFriendsAgents
+{
+    class Node
+    {
+        private float x = 0;
+        private float y = 0;
+        private float velocity_x = 0;
+        private float velocity_y = 0;
+        private bool diamond = false;
+        private bool falldown_circle = false;
+        private bool falldown_rectangle = false;
+        private Node parent;
+
+        public Node(float x, float y, float velocity_x, float velocity_y, Node parent, bool diamond)
+        {
+            this.x = x;
+            this.y = y;
+            this.velocity_x = velocity_x;
+            this.velocity_y = velocity_y;
+            this.diamond = diamond;
+            this.parent = parent;
+            this.diamond = diamond;
+        }
+
+        public override bool Equals(object obj)
+        {
+            // verifies if the object is of the correct type (the type must be Node)
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            Node n = (Node)obj;
+
+            return (get_x() == n.get_x()) && (get_y() == n.get_y()) && (has_diamond() == n.has_diamond());
+        }
+
+        /******************/
+        /*      GETs      */
+        /******************/
+
+        public float get_x()
+        {
+            return this.x;
+        }
+
+        public float get_y()
+        {
+            return this.y;
+        }
+
+        public float get_velocity_x()
+        {
+            return this.velocity_x;
+        }
+
+        public float get_velocity_y()
+        {
+            return this.velocity_y;
+        }
+
+        public bool has_diamond()
+        {
+            return this.diamond;
+        }
+
+        public bool get_falldown_circle()
+        {
+            return this.falldown_circle;
+        }
+
+        public bool get_falldown_rectangle()
+        {
+            return this.falldown_rectangle;
+        }
+
+        public Node get_parent()
+        {
+            return this.parent;
+        }
+
+        /******************/
+        /*      SETs      */
+        /******************/
+
+        public void set_x(float x)
+        {
+            this.x = x;
+        }
+
+        public void set_y(float y)
+        {
+            this.y = y;
+        }
+
+        public void set_velocity_x(float x)
+        {
+            this.x = x;
+        }
+
+        public void set_velocity_y(float y)
+        {
+            this.y = y;
+        }
+
+        public void set_diamond(bool diamond)
+        {
+            this.diamond = diamond;
+        }
+
+        public void set_falldown_circle(bool falldown_circle)
+        {
+            this.falldown_circle = falldown_circle;
+        }
+
+        public void set_falldown_rectangle(bool falldown_rectangle)
+        {
+            this.falldown_rectangle = falldown_rectangle;
+        }
+
+        public void set_parent(Node parent)
+        {
+            this.parent = parent;
+        }
+    }
+}
